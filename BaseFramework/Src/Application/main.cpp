028e8af84cdb84f51b3cc7f95fa5d1b9fb10b1a8
tanakaLv2 <kd1370757@st.kobedenshi.ac.jp>
2024-04-17 11:43:34 +0900

    BaseFramework 変更後
    all commitsの変更後

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,9 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+	//test 絶対消す
+	m_test = 2;
+
 	return true;
 }
 
